d476980e603ae659542cb02edb30c322c9664202
Eugene Chybisov <imchybisov@gmail.com>
2025-05-27 18:19:51 +0200

    chore(release): 0.3.0-beta.2

--- packages/core/src/version.ts
+++ packages/core/src/version.ts
@@ -1,2 +1,2 @@
 export const name = '@bigmi/core'
-export const version = '0.3.0-beta.1'
+export const version = '0.3.0-beta.2'
--- packages/react/src/version.ts
+++ packages/react/src/version.ts
@@ -1,2 +1,2 @@
 export const name = '@bigmi/react'
-export const version = '0.3.0-beta.1'
+export const version = '0.3.0-beta.2'
