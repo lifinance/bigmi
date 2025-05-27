3f6cf9621d62b5fe0ac20125cb0d60f14ee8356c
Eugene Chybisov <imchybisov@gmail.com>
2025-05-27 15:24:26 +0200

    chore(release): 0.3.0-beta.1

--- packages/client/src/version.ts
+++ packages/client/src/version.ts
@@ -1,2 +1,2 @@
 export const name = '@bigmi/client'
-export const version = '0.3.0-beta.0'
+export const version = '0.3.0-beta.1'
--- packages/core/src/version.ts
+++ packages/core/src/version.ts
@@ -1,2 +1,2 @@
 export const name = '@bigmi/core'
-export const version = '0.3.0-beta.0'
+export const version = '0.3.0-beta.1'
