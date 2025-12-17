b16ec2c6d27c1c2d85cd7a52ca2221221c4643ea
Eugene Chybisov <imchybisov@gmail.com>
2025-12-17 15:56:29 +0100

    chore: bump packages

--- packages/core/src/types/utils.ts
+++ packages/core/src/types/utils.ts
@@ -12,16 +12,17 @@ export type ExactPartial<type> = {
 }
 
 /** Checks if {@link type} can be narrowed further than {@link type2} */
-export type IsNarrowable<type, type2> = IsUnknown<type> extends true
-  ? false
-  : undefined extends type
+export type IsNarrowable<type, type2> =
+  IsUnknown<type> extends true
     ? false
-    : IsNever<
-          (type extends type2 ? true : false) &
-            (type2 extends type ? false : true)
-        > extends true
+    : undefined extends type
       ? false
-      : true
+      : IsNever<
+            (type extends type2 ? true : false) &
+              (type2 extends type ? false : true)
+          > extends true
+        ? false
+        : true
 
 /**
  * @internal
